1fe0da681ea3846f470e2b2390217bb5760545ec
Billy.Wang <billy0059.bw@gmail.com>
2020-08-15 21:16:12 +0800

    Add fill in template

--- demo_script_src.js
+++ demo_script_src.js
@@ -1,1 +1,20 @@
+/*
 alert("Hello World!");
+
+var inputElement = document.getElementsByTagName("input")[0];
+
+inputElement.onclick = function(){
+    var formElement = document.forms[0];
+    alert(formElement);
+}*/
+
+$(function() {
+    $('input[name="datetimes"]').daterangepicker({
+      timePicker: true,
+      startDate: moment().startOf('hour'),
+      endDate: moment().startOf('hour').add(32, 'hour'),
+      locale: {
+        format: 'M/DD hh:mm A'
+      }
+    });
+  });

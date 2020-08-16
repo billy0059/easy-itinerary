6c16a1f1483449c428200e5e2f116575a8daf65d
billy0059 <billy0059.bw@gmail.com>
2020-08-16 16:53:46 +0800

    add jquey to get the input value when clicking on submit

--- demo_script_src.js
+++ demo_script_src.js
@@ -18,3 +18,21 @@ $(function() {
       }
     });
   });
+
+/*Click submit, get the input values*/
+$(document).ready(function(){
+  $("#submit").click(function(){
+    var first_name    = $("#fname").val();
+    var last_name     = $("#lname").val();
+    var num_traveller = $("#num_traveller").val();
+    var gender        = $("#gender").val();
+    var country       = $("#country").val();
+    var budget        = $("#budget").val();
+    var date          = $("#data").val();
+    var info = "Your name : " + first_name + " " + last_name + ", traveller : " 
+                 + num_traveller + ", gender : " + gender + ", You want to travel " + country 
+                 + ". Your budget is : " + budget + ". The date : " + date;
+    alert(info);
+    $("p").html($("p").html() + " " + info);
+  });
+});
